d2d49a6cb5a11cd8e9e2be5cbcf835d39a5d0634
Maarten Sijm <M.P.Sijm@student.tudelft.nl>
2016-04-20 17:08:50 +0200

    Add Chrome to `karma.config.js`

--- karma.config.js
+++ karma.config.js
@@ -1,9 +1,23 @@
 // karma.conf.js
 module.exports = function(config) {
-  config.set({
-    basePath: './src/octopeer-github',
-    frameworks: ['qunit'],
-    files: ["main/*.js", "test/*.js"],
-    reporters: ['progress']
-  });
+    var configuration = {
+        basePath: './src/octopeer-github',
+        frameworks: ['qunit'],
+        files: ["main/*.js", "test/*.js"],
+        reporters: ['progress'],
+
+        browsers: ['Chrome', 'ChromeCanary'],
+        customLaunchers: {
+            Chrome_travis_ci: {
+                base: 'Chrome',
+                flags: ['--no-sandbox']
+            }
+        }
+    };
+
+    if (process.env.TRAVIS){
+        configuration.browsers = ['Chrome_travis_ci'];
+    }
+
+    config.set(configuration);
 };
