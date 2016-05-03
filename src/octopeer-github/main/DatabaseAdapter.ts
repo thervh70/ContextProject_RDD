fed23ff438b617577bcf0b4336173de4917fd3da
mpsijm <M.P.Sijm@student.tudelft.nl>
2016-05-03 21:23:36 +0200

    Extend DatabaseAdapter and add more tests
    The DatabaseAdapter now can dynamically get its session id.
    This is also tested in DatabaseAdapterTest.

--- src/octopeer-github/main/DatabaseAdapter.ts
+++ src/octopeer-github/main/DatabaseAdapter.ts
@@ -4,19 +4,53 @@ class DatabaseAdapter {
 
     private _url: string;
     private _session: number;
+    private _pr: number;
+    private _user: number;
     private _initialized: boolean;
 
     constructor(url: string) {
-        this._url = url;
-        if (this._url.charAt(this._url.length - 1) != "/") {
-            this._url += "/";
+        var self = this; // scope 'self' to be the adapter (instead of the AJAX call)
+        self._user = 1;
+        self._pr = 1;   // TODO The PR id should be get from the context and probably set via the constructor
+        self._session = -1;
+        self._initialized = false;
+        
+        self._url = url;
+        if (self._url.charAt(this._url.length - 1) != "/") {
+            self._url += "/";
         }
-        this._session = 1;
-        this._initialized = true;
+        
+        $.ajax(`${self.url}api/sessions/`, {
+                type: "POST",
+                dataType: "json",
+                data: {
+                    "platform": "GitHub",
+                    "pull_request": `${this.url}api/pull-requests/${this._pr}/`,
+                    "user": `${this.url}api/users/${this._user}/`
+                }
+            })
+            .done(function(data, status, jqXHR) {
+                var url = data.url.substr(0, data.url.length - 1); // trim trailing slash
+                url = url.substr(url.lastIndexOf("/") + 1); // substring starting from the character after the last "/"
+                self._session = Number(url);
+                self._initialized = true;
+            })
+            .fail(function(jqXHR, status) {
+                console.log(`DatabaseAdapter could not connect to ${self.url}.`);
+                console.log(jqXHR);
+            });
     }
 
     get url(): string {
         return this._url;
+    }
+    
+    get isInitialized(): boolean {
+        return this._initialized;
+    }
+    
+    get session(): number {
+        return this._session;
     }
 
     public post(eventType: number, start: Date, duration: number, success: JQueryPromiseCallback<any>): void {
@@ -31,7 +65,7 @@ class DatabaseAdapter {
                 }
             })
             .done(success)
-            .fail(function(jqXHR, status) {console.log(`Call failed, status: ${status}`); console.log(jqXHR);}).done();
+            .fail(function(jqXHR, status) {console.log(`Call failed, status: ${status}`); console.log(jqXHR);});
     }
 
 }
--- src/octopeer-github/test/DatabaseAdapterTest.ts
+++ src/octopeer-github/test/DatabaseAdapterTest.ts
@@ -3,21 +3,48 @@
 ///<reference path="../main/DatabaseAdapter.ts"/>
 
 describe("A DatabaseAdapter", function() {
-    it("correctly saves the url", function() {
-        expect(new DatabaseAdapter("http://localhost:8000").url).toBe("http://localhost:8000/");
-    });
-    it("can post to the API", function() {
-        var doneFn = jasmine.createSpy("success");
+    var adapter: DatabaseAdapter;
 
-        spyOn($, "ajax").and.callFake(function () {
-            var d = $.Deferred();
-            d.resolve("response");
-            return d.promise();
+    beforeEach(function() {
+        jasmine.Ajax.install();
+
+        adapter = new DatabaseAdapter("http://localhost:8000");
+        jasmine.Ajax.requests.mostRecent().respondWith({
+            status: 201,
+            contentType: "text/json",
+            responseText: '{"url":"http://localhost:8000/api/users/42/","username":"Travis"}'
+        });
+    });
+
+    afterEach(function() {
+        delete adapter;
+        jasmine.Ajax.uninstall();
+    });
+
+    it("correctly saves the url with trailing slash", function() {
+        expect(adapter.url).toBe("http://localhost:8000/");
+    });
+
+    it("correctly initializes on init", function() {
+        expect(adapter.isInitialized).toBe(true);
+    });
+
+    it("correctly sets the session number", function() {
+        expect(adapter.session).toBe(42);
+    });
+
+    it("can post to the API", function() {
+        var spyFunc = jasmine.createSpy("success");
+
+        adapter.post(1,new Date(),100,spyFunc);
+
+        jasmine.Ajax.requests.mostRecent().respondWith({
+            status: 201,
+            contentType: "text/json",
+            responseText: JSON.stringify({success:true})
         });
 
-        var adapter = new DatabaseAdapter("http://localhost:8000/");
-        adapter.post(1,new Date(),100,doneFn);
-
-        expect(doneFn).toHaveBeenCalledWith("response");
+        expect(spyFunc).toHaveBeenCalled();
+        expect(spyFunc.calls.mostRecent().args[0]).toEqual({success:true});
     });
 });
