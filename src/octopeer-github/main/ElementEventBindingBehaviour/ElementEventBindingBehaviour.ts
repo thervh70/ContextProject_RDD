a9af18c3d7c3c291b75d6685d401eebe61977e21
Youri Arkesteijn <yarkesteijn@student.tudelft.nl>
2016-05-04 13:44:16 +0200

    Added proper javadoc to interface classes

--- src/octopeer-github/main/ElementEventBindingBehaviour/ElementEventBindingBehaviour.ts
+++ src/octopeer-github/main/ElementEventBindingBehaviour/ElementEventBindingBehaviour.ts
@@ -1,11 +1,20 @@
 /**
  * Created by Youri on 03/05/2016.
  */
+
 type EventID = number;
 type EventHandler = (eventObject: JQueryEventObject) => void;
 
+/**
+ * ElementEventBindingBehaviour dictates on what events the callback should be called.
+ */
 interface ElementEventBindingBehaviour {
-    getElementId(): ElementID;
+    /**
+     * Get the ElementID
+     */
+    getElementID(): ElementID;
+    /**
+     * Get the EventID
+     */
     getEventID(): EventID;
-    
 }
--- src/octopeer-github/main/ElementSelectionBehaviour/ElementSelectionBehaviour.ts
+++ src/octopeer-github/main/ElementSelectionBehaviour/ElementSelectionBehaviour.ts
@@ -3,7 +3,26 @@
  */
 type ElementID = number;
 
+/**
+ * The element selection behaviour.
+ * this is used by the tracker and ElementEventBindingBehaviour
+ */
 interface ElementSelectionBehaviour {
+    /**
+     * Get the elementID corresponding with the database elementID.
+     */
     getElementId(): ElementID;
+
+    /**
+     * Retieve the elements this behaviour selects from the dom tree.
+     */
     getElements(): JQuery;
+
+    /**
+     * Binds the event handler into the context of the selected element.
+     * @param eventHandler the event handler to context-binding.
+     * @param context the context in which the function needs to be evaluated.
+     * @returns {EventHandler} The event handler in the given context.
+     */
+    putIntoContext(eventHandler: EventHandler, context: JQuery): EventHandler;
 }
