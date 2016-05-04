abfaf9654db4fe342a08580de476badd1b856b92
mpsijm <M.P.Sijm@student.tudelft.nl>
2016-05-04 11:02:12 +0200

    Fix incorrect reference to `typings` folder

--- src/octopeer-github/test/mainTest.ts
+++ src/octopeer-github/test/mainTest.ts
@@ -1,3 +1,5 @@
+/// <reference path="../main/main.ts"/>
+
 describe("A suite", function() {
     it("contains spec with an expectation", function() {
         expect(new Derp(42, 40).height).toBe(42);
