2def7a392dd7b6c15efd683645cd8a202ae9356d
Youri Arkesteijn <yarkesteijn@student.tudelft.nl>
2016-05-04 13:44:49 +0200

    Removed unnsearaceary refernces from SelectButtonsElementSelectionBehaviour

--- src/octopeer-github/main/ElementSelectionBehaviour/SelectButtonsElementSelectionBehaviour.ts
+++ src/octopeer-github/main/ElementSelectionBehaviour/SelectButtonsElementSelectionBehaviour.ts
@@ -1,8 +1,6 @@
 /**
  * Created by Youri on 03/05/2016.
  */
-/// <reference path="../../../../DefinitelyTyped/jquery/jquery.d.ts"/>
-/// <reference path="ElementSelectionBehaviour.ts"/>
 
 class SelectButtonsElementSelectionBehaviour implements ElementSelectionBehaviour {
     getElementId() {
