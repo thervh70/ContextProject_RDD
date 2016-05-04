117e90162ad0ca15d4caff3c0fc254b421c05178
Youri Arkesteijn <yarkesteijn@student.tudelft.nl>
2016-05-04 13:43:56 +0200

    Removed unnseaceary reference

--- src/octopeer-github/main/ElementEventBindingBehaviour/ElementEventBindingBehaviour.ts
+++ src/octopeer-github/main/ElementEventBindingBehaviour/ElementEventBindingBehaviour.ts
@@ -1,7 +1,6 @@
 /**
  * Created by Youri on 03/05/2016.
  */
-/// <reference path="../../../../DefinitelyTyped/jquery/jquery.d.ts"/>
 type EventID = number;
 type EventHandler = (eventObject: JQueryEventObject) => void;
 
