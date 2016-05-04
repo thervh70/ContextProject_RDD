21ff3a7dd297cc14cca8c334c0b97c740082245c
Maarten Sijm <M.P.Sijm@student.tudelft.nl>
2016-05-04 10:07:34 +0200

    Fix purposely failing build

--- src/octopeer-github/main/main.ts
+++ src/octopeer-github/main/main.ts
@@ -7,13 +7,13 @@ class Derp {
         this.height = height;
         this.width = width;
     }
-    
+
     public getHeight() {
         return this.height;
     }
 }
 
-var lolDerp = new Derp(42, 41);
+const lolDerp = new Derp(42, 41);
 console.log(lolDerp.width * 5);
 
 console.log("Started Octopeer");
