4382a037a0ade7dc7667d78a6e7d25ccb582d8c4
Youri Arkesteijn <yarkesteijn@student.tudelft.nl>
2016-05-06 15:56:30 +0200

    Example of binding creation

--- src/octopeer-github/main/createBindings.ts
+++ src/octopeer-github/main/createBindings.ts
@@ -1,3 +1,28 @@
-/**
- * Created by Youri on 06/05/2016.
- */
+// /**
+//  * Created by Youri on 06/05/2016.
+//  */
+// let elementEventBindingList: ElementEventBindingCreatable[];
+// let elementSelectionBindingList: ElementSelectionBehaviourCreatable[];
+// let elementEventBinding: ElementEventBindingCreatable;
+// let elementSelectionBinding: ElementSelectionBehaviourCreatable;
+// let elementEventBindingHolder: ElementEventBinding;
+// let elementSelectionBindingHolder: ElementSelectionBehaviour;
+// let databaseAdapter: DatabaseAdaptable;
+//
+// elementEventBindingList = [
+//     ClickElementEventBinding,
+// ];
+//
+// elementSelectionBindingList = [
+//     ButtonsElementSelectionBehaviour,
+// ];
+//
+// databaseAdapter = new DatabaseConsoleLogOnly();
+//
+// for (elementSelectionBinding in elementSelectionBindingList) {
+//     elementSelectionBindingHolder  = new elementSelectionBinding(databaseAdapter);
+//
+//     for (elementEventBinding in elementEventBindingList) {
+//         elementEventBindingHolder = new elementEventBinding(elementSelectionBindingHolder);
+//     }
+// }
