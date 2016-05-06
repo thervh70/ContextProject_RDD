4fee6c39043214c11b3b3cd58e094d2fb17c4106
Youri Arkesteijn <yarkesteijn@student.tudelft.nl>
2016-05-06 16:16:38 +0200

    Created a callback typeDef to cleanup the code

--- src/octopeer-github/main/DatabaseAdaptable/DatabaseAdaptable.ts
+++ src/octopeer-github/main/DatabaseAdaptable/DatabaseAdaptable.ts
@@ -6,6 +6,7 @@
 type Duration = number;
 type FileName = string;
 type LineNumber = number;
+type Callback = JQueryPromiseCallback<any>;
 
 const EMPTY_CALLBACK = function() {return; };
 EMPTY_CALLBACK(); // suppress TSLint unused-variable
@@ -14,10 +15,10 @@ interface DatabaseAdaptable {
 
     log(elementID: ElementID, eventID: EventID,
         start: Date, duration: Duration,
-        success: JQueryPromiseCallback<any>, failure: JQueryPromiseCallback<any>): void;
+        success: Callback, failure: Callback): void;
 
     logWLine(elementID: ElementID, eventID: EventID,
              filename: FileName, linenumber: LineNumber,
              start: Date, duration: Duration,
-             success: JQueryPromiseCallback<any>, failure: JQueryPromiseCallback<any>): void;
+             success: Callback, failure: Callback): void;
 }
