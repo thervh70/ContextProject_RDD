66648d5e52366c83b824cef00ba0d47d36a52852
Maarten Sijm <M.P.Sijm@student.tudelft.nl>
2016-05-06 11:13:58 +0200

    Add debug functionality to DatabaseAdapter

--- src/octopeer-github/main/DatabaseAdapter.ts
+++ src/octopeer-github/main/DatabaseAdapter.ts
@@ -5,9 +5,11 @@ class DatabaseAdapter {
     private _pr: number;
     private _user: number;
     private _initialized: boolean;
+    private _debug: boolean;
 
-    constructor(url: string) {
+    constructor(url: string, debug = false) {
         const self = this; // scope 'self' to be the adapter (instead of the AJAX call)
+        self._debug = debug;
         self._user = 1;
         self._pr = 1;   // TODO The PR id should be get from the context and probably set via the constructor
         self._session = -1;
@@ -32,11 +34,16 @@ class DatabaseAdapter {
                 suburl = suburl.substr(suburl.lastIndexOf("/") + 1);  // substring starting from the character after the last "/"
                 self._session = Number(suburl);
                 self._initialized = true;
+                if (this._debug) {
+                    console.log(`Initialized DatabaseAdapter(${url}, session=${self._session})`, self);
+                }
             })
             .fail(function(jqXHR, status) {
-                console.log(`DatabaseAdapter could not connect to ${self.url}.`);
-                console.log(jqXHR);
+                console.log(`[WARN] DatabaseAdapter could not connect to ${self.url}.`, jqXHR);
             });
+        if (this._debug) {
+            console.log(`Constructed DatabaseAdapter(${url})`, self);
+        }
     }
 
     get url(): string {
@@ -51,8 +58,13 @@ class DatabaseAdapter {
         return this._session;
     }
 
+    public setDebug(d = true) {
+        this._debug = d;
+    }
+
     public post(eventType: number, start: Date, duration: number, success: JQueryPromiseCallback<any>): void {
-        $.ajax(`${this.url}api/events/`, {
+        const self = this;
+        const ajax = $.ajax(`${this.url}api/events/`, {
                 data: {
                     "started_at": start.toJSON(),
                     "duration": duration,
@@ -62,10 +74,18 @@ class DatabaseAdapter {
                 dataType: "json",
                 type: "POST",
             })
-            .done(success)
+            .done(function(data, status, jqXHR) {
+                if (self._debug) {
+                    console.log(`Call success, status: ${status}`, jqXHR);
+                }
+                success(data, status, jqXHR);
+            })
             .fail(function(jqXHR, status) {
-                console.log(`Call failed, status: ${status}`); console.log(jqXHR);
+                console.log(`[WARN] Call failed, status: ${status}`, jqXHR);
             });
+        if (this._debug) {
+            console.log("Sent out AJAX request: ", ajax);
+        }
     }
 
 }
--- src/octopeer-github/test/DatabaseAdapterTest.ts
+++ src/octopeer-github/test/DatabaseAdapterTest.ts
@@ -45,4 +45,21 @@ describe("A DatabaseAdapter", function() {
         expect(spyFunc).toHaveBeenCalled();
         expect(spyFunc.calls.mostRecent().args[0]).toEqual({success: true});
     });
+
+    it("can be set to debug mode", function() {
+        const consoleSpy = spyOn(console, "log");
+
+        adapter.setDebug();
+
+        adapter.post(1, new Date(), 100, function(data) {return data; });
+        jasmine.Ajax.requests.mostRecent().respondWith({
+            contentType: "text/json",
+            responseText: JSON.stringify({success: true}),
+            status: 201,
+        });
+
+        expect(consoleSpy).toHaveBeenCalledTimes(3);
+        expect(consoleSpy.calls.all()[0].args[0]).toBe("Sent out AJAX request: ");
+        expect(consoleSpy.calls.all()[1].args[0]).toBe("Call success, status: success");
+    });
 });
