16daef0bc9f840ae808bdffeb2b2baf0760f2395
Youri Arkesteijn <yarkesteijn@student.tudelft.nl>
2016-05-08 11:22:27 +0200

    Creation of ElementID class

---
+++ src/octopeer-github/main/ElementID.ts
@@ -0,0 +1,21 @@
+/**
+ * Created by Youri on 08/05/2016.
+ */
+/**
+ * Class to hold elementID's so they can be a semanticaly different datatype.
+ */
+class ElementID {
+    /**
+     * Create a new ElementID object.
+     * @param elementID the element id to hold.
+     */
+    constructor(private elementID: number) {}
+
+    /**
+     * get the value of the element ID.
+     * @returns {number}
+     */
+    get getElementID() {
+        return this.elementID;
+    }
+}
