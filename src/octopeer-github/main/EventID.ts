3fa1f1f488ba7634d107668ff7810a20992f686f
Youri Arkesteijn <yarkesteijn@student.tudelft.nl>
2016-05-08 11:22:40 +0200

    Creation of EventID class

---
+++ src/octopeer-github/main/EventID.ts
@@ -0,0 +1,21 @@
+/**
+ * Created by Youri on 08/05/2016.
+ */
+/**
+ * Class to hold elementID's so they can be a semanticaly different datatype.
+ */
+class EventID {
+    /**
+     * Create a new EventID object.
+     * @param eventID the event id to hold.
+     */
+    constructor(private eventID: number) {}
+
+    /**
+     * get the value of the element ID.
+     * @returns {number}
+     */
+    get getEventID() {
+        return this.eventID;
+    }
+}
