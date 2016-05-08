120aed18bd226d8860a8ff8390adf97baabf3758
Youri Arkesteijn <yarkesteijn@student.tudelft.nl>
2016-05-08 14:07:17 +0200

    Added EventID tests

---
+++ src/octopeer-github/test/EventIDTest.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by Youri on 08/05/2016.
+ */
+const numericalValueforEventID = 4; // randomly chosen.
+
+describe("An Idenfifier for an event", function() {
+    let eventID: EventID;
+
+    it("should return the value of instantiation", function () {
+        eventID = new EventID(numericalValueforEventID);
+        expect(eventID.getElementID).toBe(numericalValueforEventID);
+    });
+});
