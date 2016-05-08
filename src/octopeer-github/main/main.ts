773e09f1478ad395d5c08dd93e2415d046251b1a
mpsijm <M.P.Sijm@student.tudelft.nl>
2016-05-08 14:35:00 +0200

    #24 Add Controller and let it run when github.com is loaded

--- src/octopeer-github/main/main.ts
+++ src/octopeer-github/main/main.ts
@@ -1,15 +1,5 @@
-/// <reference path="DatabaseAdaptable/DatabaseAdaptable.ts"/>
-/// <reference path="DatabaseAdaptable/DatabaseConsoleLogOnly.ts"/>
-/// <reference path="ElementSelectionBehaviour/ElementSelectionBehaviour.ts"/>
-/// <reference path="ElementSelectionBehaviour/ButtonsElementSelectionBehaviour.ts"/>
-/// <reference path="ElementEventBinding/ElementEventBinding.ts"/>
-/// <reference path="ElementEventBinding/ClickElementEventBinding.ts"/>
-
-let databaseAdapterInterfaceImplementer: DatabaseAdaptable;
-let buttonSelector: ElementSelectionBehaviour;
-let buttonClickedLogging: ElementEventBinding;
-
-databaseAdapterInterfaceImplementer = new DatabaseConsoleLogOnly();
-buttonSelector = new ButtonsElementSelectionBehaviour(databaseAdapterInterfaceImplementer);
-buttonClickedLogging = new ClickElementEventBinding(buttonSelector);
-
+$(document).ready(function() {
+    let controller = new Controller();
+    console.log(controller); // suppress unused
+});
+console.log(document);
