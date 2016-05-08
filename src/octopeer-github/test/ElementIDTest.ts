08d10795897ff7a5fcb9d3b4ce20d0eab87f8784
Youri Arkesteijn <yarkesteijn@student.tudelft.nl>
2016-05-08 14:05:59 +0200

    Added elementID test

---
+++ src/octopeer-github/test/ElementIDTest.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by Youri on 08/05/2016.
+ */
+const numericalValueforElementID = 4; // randomly chosen.
+
+describe("An Idenfifier for an element", function() {
+    let elementID: ElementID;
+
+    it("should return the value of instantiation", function () {
+        elementID = new ElementID(numericalValueforElementID);
+        expect(elementID.getElementID).toBe(numericalValueforElementID);
+    });
+});
