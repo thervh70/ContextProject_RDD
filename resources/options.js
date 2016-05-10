7add85b1cee24b3b712682ecca5d68ca429c0bcc
mdingjan <M.J.C.Dingjan@student.tudelft.nl>
2016-05-10 18:50:45 +0200

    #25 Finalized the submit button
    When the submit button is pressed, all settings will be saved to the chrome.storage.
    When the page is refreshed or visited at a later point in time, these saved settings will be loaded.

--- resources/options.js
+++ resources/options.js
@@ -1,26 +1,61 @@
 /**
- * Contains necessary functions for the options.
+ * Created by Mitchell on 10-5-2016.
+ * Contains necessary functions for the Settings page of Octopeer.
  */
 
-// Saves options to chrome.storage
+// Saves options to chrome.storage.
 function save_options() {
     var logging = document.getElementById('logging').checked;
+    var tabs = document.getElementById('tabs').checked;
+    var comments = document.getElementById('comments').checked;
+    var peer_comments = document.getElementById('peer_comments').checked;
+    var focus = document.getElementById('focus').checked;
+    var username = document.getElementById('username').checked;
+    var repo = document.getElementById('repo').checked;
+    var file = document.getElementById('file').checked;
     chrome.storage.sync.set({
-        loggingEnabled: logging
+        // General
+        loggingEnabled: logging,
+        // Privacy
+        trackTabs: tabs,
+        trackComments: comments,
+        trackPeerComments: peer_comments,
+        trackFocus: focus,
+        // Security
+        hashUsername: username,
+        hashRepo: repo,
+        hashFile: file
+        // Hints
     }, function() {
         Materialize.toast("Settings saved!",2000);
     });
 }
 
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
+// Restores the states of the checkboxes, using the preferences stored in chrome.storage.
 function restore_options() {
     chrome.storage.sync.get({
-        loggingEnabled: true
+        // Default values
+        loggingEnabled: true,
+        trackTabs: true,
+        trackComments: true,
+        trackPeerComments: true,
+        trackFocus: true,
+        hashUsername: true,
+        hashRepo: true,
+        hashFile: false
     }, function(items) {
+        // Saved values from the chrome.storage
         document.getElementById('logging').checked = items.loggingEnabled;
+        document.getElementById('tabs').checked = items.trackTabs;
+        document.getElementById('comments').checked = items.trackComments;
+        document.getElementById('peer_comments').checked = items.trackPeerComments;
+        document.getElementById('focus').checked = items.trackFocus;
+        document.getElementById('username').checked = items.hashUsername;
+        document.getElementById('repo').checked = items.hashRepo;
+        document.getElementById('file').checked = items.hashFile;
     });
 }
+
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click',
     save_options);
