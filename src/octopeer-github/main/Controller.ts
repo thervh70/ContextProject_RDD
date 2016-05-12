b7f7a4143fd567907e2dadc8a146b70e61e3eeb0
Maarten Sijm <M.P.Sijm@student.tudelft.nl>
2016-05-12 13:30:33 +0200

    Let Extension query existing tabs on startup

--- src/octopeer-github/main/Controller.ts
+++ src/octopeer-github/main/Controller.ts
@@ -6,6 +6,7 @@
 /// <reference path="ElementEventBinding/ElementEventBinding.ts"/>
 /// <reference path="ElementEventBinding/ClickElementEventBinding.ts"/>
 
+import Tab = chrome.tabs.Tab;
 /**
  * The Controller hooks the event handlers to the DOM-tree.
  */
@@ -46,32 +47,54 @@ class Controller {
         }
     }
 
+    /**
+     * Set up all event handlers in the Chrome API.
+     */
     private connectToContentScript() {
         const self = this;
-        // format:       http[s]//[...]github.com/<owner>/<repo>/pull/<pr-no>[/...]
-        let urlFormat = /https?:\/\/.*github\.com\/(.+)\/(.+)\/pull\/([^\/]+)\/?.*/;
-        // Whenever a tab updates, send a message to re-hook to DOM
-        chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-            if (changeInfo.url && urlFormat.test(changeInfo.url)) {
-                let urlInfo = urlFormat.exec(changeInfo.url);
-                console.log(`Owner: ${urlInfo[1]}, Repo: ${urlInfo[2]}, PR-number: ${urlInfo[3]}`);
-                console.log("Tab:", tabId, tab, "ChangeInfo: ", changeInfo);
-                chrome.tabs.sendMessage(tabId, {
-                    hookToDom: true,
-                }, function(result) {
-                    console.log(result);
-                });
+        // On start-up, let all tabs hook to DOM
+        chrome.tabs.query({}, function(tabs) {
+            let tab: Tab;
+            for (tab of tabs) {
+                self.testAndSend(tab);
             }
         });
+        // Whenever a tab updates, send a message to re-hook to DOM
+        chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+            if (changeInfo.status && changeInfo.status === "complete") {
+                self.testAndSend(tab);
+            }
+        });
+        // When a tab sends a message, log it to the Database
         chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
             self.database.post(message, function() {
-                console.log("Successfully logged to database: ", message);
+                console.log("[Database] Successfully logged to database: ", message);
             }, function() {
                 console.log("[WARN] Log to database of following object failed: ", message);
                 console.log("[WARN] Original sender: ", sender);
             });
             sendResponse({});
         });
+    }
+
+    /**
+     * Only sends a message to a tab if they contain the correct URL.
+     * @param tab   the Tab to check for
+     */
+    private testAndSend(tab: Tab) {
+        const url = tab.url;
+        // format:         http[s]//[...]github.com/(owner)/(repo)/pull/(pr-no)[/...]
+        const urlFormat = /https?:\/\/.*github\.com\/(.+)\/(.+)\/pull\/([^\/]+)\/?.*/;
+        if (urlFormat.test(url)) {
+            let urlInfo = urlFormat.exec(url);
+            console.log(`[Tab] Owner: ${urlInfo[1]}, Repo: ${urlInfo[2]}, PR-number: ${urlInfo[3]}`);
+            chrome.tabs.sendMessage(tab.id, {
+                hookToDom: true,
+            }, function(result) {
+                let str = result || `should be refreshed because content script is not loaded (${tab.url})`;
+                console.log(`[Tab] ${str}`);
+            });
+        }
     }
 
 }
--- src/octopeer-github/main/main.ts
+++ src/octopeer-github/main/main.ts
@@ -4,19 +4,22 @@ if (inExtension) {
     const controller = new Controller();
     controller.start();
 } else {
-    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-        "use strict";
-        console.log(request, sender);
-        if (request.hookToDom) {
-            new Controller().hookToDOM({
-                post: function(data: EventObject, success: Callback, failure: Callback) {
-                    chrome.runtime.sendMessage(data);
-                    success();
-                },
-            });
-            sendResponse(`Tab hooked to DOM (${location.href})`);
-        } else {
-            sendResponse(`Tab did noting (${location.href})`);
-        }
-    });
+    let hookedToDOM = false;
+    if (!chrome.runtime.onMessage.hasListeners()) {
+        chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+            "use strict";
+            if (request.hookToDom && !hookedToDOM) {
+                hookedToDOM = true;
+                new Controller().hookToDOM({
+                    post: function (data: EventObject, success: Callback, failure: Callback) {
+                        chrome.runtime.sendMessage(data);
+                        success();
+                    },
+                });
+                sendResponse(`hooked to DOM (${location.href})`);
+            } else {
+                sendResponse(`did noting (${location.href})`);
+            }
+        });
+    }
 }
