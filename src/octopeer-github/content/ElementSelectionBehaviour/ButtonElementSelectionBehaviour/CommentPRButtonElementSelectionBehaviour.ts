d477274b116fa68d1b40006e27dbc997badf2228
Mathias Meuleman <m.meuleman@student.tudelft.nl>
2016-05-13 01:05:21 +0200

    Added missing Element

---
+++ src/octopeer-github/content/ElementSelectionBehaviour/ButtonElementSelectionBehaviour/CommentPRButtonElementSelectionBehaviour.ts
@@ -0,0 +1,17 @@
+/// <reference path="ButtonElementSelectionBehaviour.ts"/>
+/**
+ * Created by Mathias on 2016-05-13.
+ * Class for selecting the ClosePR button
+ */
+class CommentPRButtonElementSelectionBehaviour extends ButtonElementSelectionBehaviour {
+
+    /**
+     * Constructor of this class.
+     * @param database The database that is used to store the Elements.
+     */
+    public constructor(database: DatabaseAdaptable) {
+        super(database);
+        this.elementDsc = "..timeline-comment button.btn-primary";
+        this.elementID = new ElementID(113);
+    }
+}
