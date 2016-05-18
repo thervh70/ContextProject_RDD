ccf221d116ed0a7d5af499ca7a546a11a98e44fe
mdingjan <M.J.C.Dingjan@student.tudelft.nl>
2016-05-18 21:33:56 +0200

    #49 Set-up tools that are needed
    There is still a problem: some kind of error is thrown when initializing.

--- karma.config.js
+++ karma.config.js
@@ -3,16 +3,27 @@ module.exports = function(config) {
     var configuration = {
         basePath: '.',
         frameworks: ['jasmine'],
+        plugins: ['karma-remap-istanbul'],
         files: [
             "node_modules/jquery/dist/jquery.min.js",
             "node_modules/jasmine-ajax/lib/mock-ajax.js",
             "node_modules/jasmine-jquery/lib/jasmine-jquery.js",
             "build/test.js"
         ],
-        reporters: ['progress', 'coverage'],
+        reporters: ['progress', 'coverage', 'karma-remap-istanbul'],
         coverageReporter: {
             type: 'html',
             dir: 'coverage/'
+        },
+
+        remapIstanbulReporter: {
+            src: 'coverage/report.json',
+            reports: {
+                lcovonly: 'coverage/',
+                html: 'coverage/'
+            },
+            timeoutNotCreated: 1000, // default value
+            timeoutNoMoreFiles: 1000 // default value
         },
 
         preprocessors: {
