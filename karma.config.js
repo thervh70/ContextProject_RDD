6d8465ca5a5da69c2b70e03efb74dec1bde9cdb9
Maarten Sijm <M.P.Sijm@student.tudelft.nl>
2016-05-19 09:20:34 +0200

    #49 Fix coverage reporter locally

--- karma.config.js
+++ karma.config.js
@@ -3,14 +3,13 @@ module.exports = function(config) {
     var configuration = {
         basePath: '.',
         frameworks: ['jasmine'],
-        // plugins: ['karma-remap-istanbul'],
         files: [
             "node_modules/jquery/dist/jquery.min.js",
             "node_modules/jasmine-ajax/lib/mock-ajax.js",
             "node_modules/jasmine-jquery/lib/jasmine-jquery.js",
             "build/test.js"
         ],
-        reporters: ['progress', 'coverage', 'karma-remap-istanbul'],
+        reporters: ['progress', 'coverage'/*, 'karma-remap-istanbul'*/],
         coverageReporter: {
             type : 'json',
             subdir : '.',
@@ -20,21 +19,10 @@ module.exports = function(config) {
             }
         },
 
-        // remapIstanbulReporter: {
-        //     src: 'coverage/coverage-final.json',
-        //     reports: {
-        //         lcovonly: 'coverage/',
-        //         html: 'coverage/'
-        //     },
-        //     timeoutNotCreated: 1000, // default value
-        //     timeoutNoMoreFiles: 1000 // default value
-        // },
-
         preprocessors: {
             // source files, that you wanna generate coverage for
             // do not include tests or libraries
-            'build/content.js': ['coverage'],
-            'build/main.js': ['coverage']
+            'build/test.js': ['coverage']
         },
 
         browsers: ['Chrome', 'ChromeCanary'],
