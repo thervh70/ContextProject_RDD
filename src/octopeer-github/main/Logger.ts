02360e2f258632d184385611a12ff70535d0a74b
Maarten Sijm <M.P.Sijm@student.tudelft.nl>
2016-05-19 15:23:47 +0200

    #33 Add JSDoc to Logger

--- src/octopeer-github/main/Logger.ts
+++ src/octopeer-github/main/Logger.ts
@@ -2,28 +2,53 @@
  * Created by Maarten on 17-05-2016.
  */
 
+/**
+ * The Logger class (singleton) is used to log events to the console.
+ * The Logger will not output debug messages if this is not explicitly requested.
+ */
 const Logger = new (class Logger {
 
     private isDebug = false;
 
+    /**
+     * Logs a warning to the console.
+     * @param obj The object to be logged.
+     */
     public warn(obj: any) {
         this.log("WARN ", obj);
     }
 
+    /**
+     * Logs a debug message to the console.
+     * @param obj The object to be logged.
+     */
     public debug(obj: any) {
         if (this.isDebug) {
             this.log("DEBUG", obj);
         }
     }
 
+    /**
+     * Logs a database post to the console. (only used by ConsoleLogDatabaseAdapter)
+     * @param obj The object to be logged.
+     */
     public database(obj: any) {
         this.log("DATA ", obj);
     }
 
+    /**
+     * If debug = true, the Logger will log debug messages.
+     * @state state=true The new debug state.
+     */
     public setDebug(state = true) {
         this.isDebug = state;
     }
 
+    /**
+     * Logs a warning to the console.
+     * @param tag The tag that this log will get.
+     * @param obj The object to be logged.
+     */
     private log(tag: string, obj: any) {
         console.log(`[${tag}] `, obj);
     }
