1648a9e77f15e8b7642931d80384d2fe0b0869ca
Maarten Sijm <M.P.Sijm@student.tudelft.nl>
2016-05-20 12:48:01 +0200

    #44 Rewrite Options from namespace to closure singleton

--- src/octopeer-github/main/Options.ts
+++ src/octopeer-github/main/Options.ts
@@ -3,31 +3,31 @@
  * Class from which user options can be retrieved.
  * These options have been maintained by the front-end.
  */
-namespace Options {
-    let logging: Boolean;
-    let tabs: Boolean;
-    let comments: Boolean;
-    let peer_comments: Boolean;
-    let focus: Boolean;
-    let username: Boolean;
-    let repo: Boolean;
-    let file: Boolean;
+const Options = new (class Options {
+    private logging: Boolean;
+    private tabs: Boolean;
+    private comments: Boolean;
+    private peer_comments: Boolean;
+    private focus: Boolean;
+    private username: Boolean;
+    private repo: Boolean;
+    private file: Boolean;
 
     /**
      * Initialization fetches the current settings and stores them in this class.
      */
-    export function init() {
+    public init() {
         chrome.storage.sync.get(["loggingEnabled", "trackTabs", "trackComments", "trackPeerComments",
         "trackFocus", "hashUsername", "hashRepo", "hashFile"], function (obj) {
             const object = <any> obj;
-            logging = object.loggingEnabled;
-            tabs = object.trackTabs;
-            comments = object.trackComments;
-            peer_comments = object.trackPeerComments;
-            focus = object.trackFocus;
-            username = object.hashUsername;
-            repo = object.hashRepo;
-            file = object.hashFile;
+            this.logging = object.loggingEnabled;
+            this.tabs = object.trackTabs;
+            this.comments = object.trackComments;
+            this.peer_comments = object.trackPeerComments;
+            this.focus = object.trackFocus;
+            this.username = object.hashUsername;
+            this.repo = object.hashRepo;
+            this.file = object.hashFile;
         });
     }
 
@@ -35,18 +35,18 @@ namespace Options {
      * Enables a listener that listens for changes in the sync storage area.
      * This means that any items that was changed (newValue) is set if changed;
      */
-    export function update() {
+    public update() {
         chrome.storage.onChanged.addListener(function (changes, areaName) {
             if (areaName === "sync") {
                 const changeObject = <any> changes;
-                logging = changeObject.loggingEnabled ? changeObject.loggingEnabled.newValue : logging;
-                tabs = changeObject.trackTabs ? changeObject.trackTabs.newValue : tabs;
-                comments = changeObject.trackComments ? changeObject.trackComments.newValue : comments;
-                peer_comments = changeObject.trackPeerComments ? changeObject.trackPeerComments.newValue : peer_comments;
-                focus = changeObject.trackFocus ? changeObject.trackFocus.newValue : focus;
-                username = changeObject.hashUsername ? changeObject.hashUsername.newValue : username;
-                repo = changeObject.hashRepo ? changeObject.hashRepo.newValue : repo;
-                file = changeObject.hashFile ? changeObject.hashFile.newValue : file;
+                this.logging = changeObject.loggingEnabled ? changeObject.loggingEnabled.newValue : this.logging;
+                this.tabs = changeObject.trackTabs ? changeObject.trackTabs.newValue : this.tabs;
+                this.comments = changeObject.trackComments ? changeObject.trackComments.newValue : this.comments;
+                this.peer_comments = changeObject.trackPeerComments ? changeObject.trackPeerComments.newValue : this.peer_comments;
+                this.focus = changeObject.trackFocus ? changeObject.trackFocus.newValue : this.focus;
+                this.username = changeObject.hashUsername ? changeObject.hashUsername.newValue : this.username;
+                this.repo = changeObject.hashRepo ? changeObject.hashRepo.newValue : this.repo;
+                this.file = changeObject.hashFile ? changeObject.hashFile.newValue : this.file;
             }
         });
     }
@@ -55,63 +55,64 @@ namespace Options {
      * Gets the logging preference, from the chrome storage.
      * @returns {Boolean}
      */
-    export function getLogging() {
-        return logging;
+    public getLogging() {
+        return this.logging;
     }
 
     /**
      * Gets the tab tracking preference, from the chrome storage.
      * @returns {Boolean}
      */
-    export function getTabs() {
-        return tabs;
+    public getTabs() {
+        return this.tabs;
     }
 
     /**
      * Gets the comment tracking preference, from the chrome storage.
      * @returns {Boolean}
      */
-    export function getComments() {
-        return comments;
+    public getComments() {
+        return this.comments;
     }
 
     /**
      * Gets the peer comments tracking preference, from the chrome storage.
      * @returns {Boolean}
      */
-    export function getPeerComments() {
-        return peer_comments;
+    public getPeerComments() {
+        return this.peer_comments;
     }
 
     /**
      * Gets the focus tracking preference, from the chrome storage.
      * @returns {Boolean}
      */
-    export function getFocus() {
-        return focus;
+    public getFocus() {
+        return this.focus;
     }
 
     /**
      * Gets the username encryption preference, from the chrome storage.
      * @returns {Boolean}
      */
-    export function getUsername() {
-        return username;
+    public getUsername() {
+        return this.username;
     }
 
     /**
      * Gets the repository encryption preference, from the chrome storage.
      * @returns {Boolean}
      */
-    export function getRepo() {
-        return repo;
+    public getRepo() {
+        return this.repo;
     }
 
     /**
      * Gets the file encryption preference, from the chrome storage.
      * @returns {Boolean}
      */
-    export function getFile() {
-        return file;
+    public getFile() {
+        return this.file;
     }
-}
+})();
+Options.getLogging(); // Suppress unused variable
