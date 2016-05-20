4f897a4d21f2fa1f49b2fb80759d788a0214aebb
Youri Arkesteijn <yarkesteijn@student.tudelft.nl>
2016-05-20 11:21:27 +0200

    DoNotWatchSettings are now derived from the Settings class instead of there own declaration

--- src/octopeer-github/main/Settings/DoNotWatchSettings.ts
+++ src/octopeer-github/main/Settings/DoNotWatchSettings.ts
@@ -1,3 +1,41 @@
+/// <reference path="Settings.ts"/>
+/// <reference path="../../content/ElementEventBinding/ElementEventBinding.ts"/>
+/// <reference path="../../content/ElementEventBinding/ClickElementEventBinding.ts"/>
+/// <reference path="../../content/ElementEventBinding/KeystrokeElementEventBinding.ts"/>
+/// <reference path="../../content/ElementEventBinding/MouseEnterElementEventBinding.ts"/>
+/// <reference path="../../content/ElementEventBinding/MouseLeaveElementEventBinding.ts"/>
+/// <reference path="../../content/ElementEventBinding/ScrollIntoViewElementEventBinding.ts"/>
+/// <reference path="../../content/ElementEventBinding/ScrollOutOfViewElementEventBinding.ts"/>
+/// <reference path="../../content/ElementSelectionBehaviour/ElementSelectionBehaviour.ts"/>
+/// <reference path="../../content/ElementSelectionBehaviour/AbstractElementSelectionBehaviour.ts"/>
+/// <reference path="../../content/ElementSelectionBehaviour/ButtonElementSelectionBehaviour/AddEmoticonButtonElementSelectionBehaviour.ts"/>
+/// <reference path="../../content/ElementSelectionBehaviour/ButtonElementSelectionBehaviour/CancelEditPRNameButtonElementSelectionBehaviour.ts"/>
+/// <reference path="../../content/ElementSelectionBehaviour/ButtonElementSelectionBehaviour/CancelInlineCommentButtonElementSelectionBehaviour.ts"/>
+/// <reference path="../../content/ElementSelectionBehaviour/ButtonElementSelectionBehaviour/CheckDetailButtonElementSelectionBehaviour.ts"/>
+/// <reference path="../../content/ElementSelectionBehaviour/ButtonElementSelectionBehaviour/ClosePRButtonElementSelectionBehaviour.ts"/>
+/// <reference path="../../content/ElementSelectionBehaviour/ButtonElementSelectionBehaviour/CommentInlineCommentButtonElementSelectionBehaviour.ts"/>
+/// <reference path="../../content/ElementSelectionBehaviour/ButtonElementSelectionBehaviour/CommentPRButtonElementSelectionBehaviour.ts"/>
+/// <reference path="../../content/ElementSelectionBehaviour/ButtonElementSelectionBehaviour/EditCommentButtonElementSelectionBehaviour.ts"/>
+/// <reference path="../../content/ElementSelectionBehaviour/ButtonElementSelectionBehaviour/EditPRNameButtonElementSelectionBehaviour.ts"/>
+/// <reference path="../../content/ElementSelectionBehaviour/ButtonElementSelectionBehaviour/InlineCommentButtonElementSelectionBehaviour.ts"/>
+/// <reference path="../../content/ElementSelectionBehaviour/ButtonElementSelectionBehaviour/MergePRButtonElementSelectionBehaviour.ts"/>
+/// <reference path="../../content/ElementSelectionBehaviour/ButtonElementSelectionBehaviour/SaveEditPRNameButtonElementSelectionBehaviour.ts"/>
+/// <reference path="../../content/ElementSelectionBehaviour/ButtonElementSelectionBehaviour/ShowChecksToggleButtonElementSelectionBehaviour.ts"/>
+/// <reference path="../../content/ElementSelectionBehaviour/MiscellaneousElementSelectionBehaviour/DateMiscellaneousElementSelectionBehaviour.ts"/>
+/// <reference path="../../content/ElementSelectionBehaviour/NameElementSelectionBehaviour/CommitHashcodeNameElementSelectionBehaviour.ts"/>
+/// <reference path="../../content/ElementSelectionBehaviour/NameElementSelectionBehaviour/CommitMessageNameElementSelectionBehaviour.ts"/>
+/// <reference path="../../content/ElementSelectionBehaviour/NameElementSelectionBehaviour/OtherContributerNameElementSelectionBehaviour.ts"/>
+/// <reference path="../../content/ElementSelectionBehaviour/NameElementSelectionBehaviour/PRCreatorNameElementSelectionBehaviour.ts"/>
+/// <reference path="../../content/ElementSelectionBehaviour/NameElementSelectionBehaviour/PRParticipantNameElementSelectionBehaviour.ts"/>
+/// <reference path="../../content/ElementSelectionBehaviour/SettingElementSelectionBehaviour/AssigneeSettingElementSelectionBehaviour.ts"/>
+/// <reference path="../../content/ElementSelectionBehaviour/SettingElementSelectionBehaviour/LabelSettingElementSelectionBehaviour.ts"/>
+/// <reference path="../../content/ElementSelectionBehaviour/SettingElementSelectionBehaviour/LockConversationSettingElementSelectionBehaviour.ts"/>
+/// <reference path="../../content/ElementSelectionBehaviour/SettingElementSelectionBehaviour/MilestoneSettingElementSelectionBehaviour.ts"/>
+/// <reference path="../../content/ElementSelectionBehaviour/SettingElementSelectionBehaviour/UnsubscribeSettingElementSelectionBehaviour.ts"/>
+/// <reference path="../../content/ElementSelectionBehaviour/TabHeaderElementSelectionBehaviour/CommitsTabHeaderElementSelectionBehaviour.ts"/>
+/// <reference path="../../content/ElementSelectionBehaviour/TabHeaderElementSelectionBehaviour/ConversationTabHeaderElementSelectionBehaviour.ts"/>
+/// <reference path="../../content/ElementSelectionBehaviour/TabHeaderElementSelectionBehaviour/FilesChangedTabHeaderElementSelectionBehaviour.ts"/>
+
 /**
  * Created by Youri on 19-5-2016.
  */
@@ -14,39 +52,38 @@ type ElementXEventCreatable = {
  * Class for indicating all internal settings of the application.
  */
 const DoNotWatchSettings = new (class DoNotWatchSettings {
-    private doNotWatchElements: ElementSelectionBehaviourCreatable[];
-    private doNotWatchEvents: ElementEventBindingCreatable[];
-    private doNotWatchCombination: ElementXEventCreatable[];
-
     /**
      * Gets Elements not to Log, from the chrome storage.
      * @returns {ElementSelectionBehaviourCreatable[]}
      */
     public getElements() {
-        chrome.storage.sync.get("doNotWatchElements", function (obj: ElementSelectionBehaviourCreatable[]) {
-            this.doNotWatchElements = obj;
-        });
-        return this.doNotWatchElements;
+        let doNotWatchElements: ElementSelectionBehaviourCreatable[];
+
+        return doNotWatchElements;
     }
     /**
      * Gets Events not to Log, from the chrome storage.
      * @returns {ElementSelectionBehaviourCreatable[]}
      */
     public getEvents() {
-        chrome.storage.sync.get("doNotWatchEvents", function (obj: ElementEventBindingCreatable[]) {
-            this.doNotWatchEvents = obj;
-        });
-        return this.doNotWatchEvents;
+        let doNotWatchEvents: ElementEventBindingCreatable[];
+        doNotWatchEvents = [];
+
+        if (Settings.getFocus()) {
+            doNotWatchEvents.push(ScrollIntoViewElementEventBinding);
+            doNotWatchEvents.push(ScrollOutOfViewElementEventBinding);
+        }
+
+        return doNotWatchEvents;
     }
     /**
      * Gets Elements not to Log, from the chrome storage.
      * @returns {ElementSelectionBehaviourCreatable[]}
      */
     public getCombinations() {
-        chrome.storage.sync.get("doNotWatchCombination", function (obj: ElementXEventCreatable[]) {
-            this.doNotWatchCombination = obj;
-        });
-        return this.doNotWatchCombination;
+        let doNotWatchCombination: ElementXEventCreatable[];
+
+        return doNotWatchCombination;
     }
 })();
 DoNotWatchSettings.getElements(); // supress unused variable warning
