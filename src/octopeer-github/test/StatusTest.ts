82d149339435f94be25ce27e7bf7fc01a0273c12
mdingjan <M.J.C.Dingjan@student.tudelft.nl>
2016-05-23 11:51:18 +0200

    #67 Added tests for the getIcon function

--- src/octopeer-github/test/StatusTest.ts
+++ src/octopeer-github/test/StatusTest.ts
@@ -4,7 +4,24 @@
 
 describe("The Status", function() {
 
+    // getIcon tests
     it("should return the path of the 'off' icon by default", function () {
         expect(Status.getIcon()).toBe("img/icon/off.png");
+    });
+
+    it("should return the path of the 'running' icon when the status code is 'running'", function () {
+        expect(Status.getIcon(StatusCode.RUNNING)).toBe("img/icon/running.png");
+    });
+
+    it("should return the path of the 'error' icon when the status code is 'error'", function () {
+        expect(Status.getIcon(StatusCode.ERROR)).toBe("img/icon/error.png");
+    });
+
+    it("should return the path of the 'standby' icon when the status code is 'standby'", function () {
+        expect(Status.getIcon(StatusCode.STANDBY)).toBe("img/icon/standby.png");
+    });
+
+    it("should return the path of the un icon when the status code is 'off'", function () {
+        expect(Status.getIcon(StatusCode.OFF, 19)).toBe("img/icon/off19.png");
     });
 });
