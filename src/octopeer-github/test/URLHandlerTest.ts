c457cd78bf9c320f03c372ce01d6ae67361dd7ff
Maarten Sijm <M.P.Sijm@student.tudelft.nl>
2016-05-24 19:46:50 +0200

    Refactor isPullRequestUrl tests

--- src/octopeer-github/test/URLHandlerTest.ts
+++ src/octopeer-github/test/URLHandlerTest.ts
@@ -22,32 +22,21 @@ describe("The URLHandler", function() {
 
     describe("should, while checking if a URL is from a PR,", function() {
         const testURL = "https://github.com/thervh70/ContextProject_RDD/pull/7";
-        it("be correct", function() {
-            expect(URLHandler.isPullRequestUrl(testURL)).toEqual([
-                testURL, "thervh70", "ContextProject_RDD", "7",
-            ]);
-        });
-
-        it("be correct with a trailing slash", function() {
-            const url = testURL + "/";
-            expect(URLHandler.isPullRequestUrl(url)).toEqual([
-                url, "thervh70", "ContextProject_RDD", "7",
-            ]);
-        });
-
-        it("be correct when in the files tab", function() {
-            const url = testURL + "/files";
-            expect(URLHandler.isPullRequestUrl(url)).toEqual([
-                url, "thervh70", "ContextProject_RDD", "7",
-            ]);
-        });
-
-        it("be correct when linked to a specific event", function() {
-            const url = testURL + "#event-666986359";
-            expect(URLHandler.isPullRequestUrl(url)).toEqual([
-                url, "thervh70", "ContextProject_RDD", "7",
-            ]);
-        });
+        const suffices = ["", "/", "/files", "#event-666986359"];
+        const messages = [
+            "without a trailing slash",
+            "with a trailing slash",
+            "when in the files tab",
+            "when linked to a specific event",
+        ];
+        for (let i = 0; i < suffices.length; i++) {
+            it("be correct " + messages[i], function () {
+                const url = testURL + suffices[i];
+                expect(URLHandler.isPullRequestUrl(url)).toEqual([
+                    url, "thervh70", "ContextProject_RDD", "7",
+                ]);
+            });
+        }
     });
 
 });
