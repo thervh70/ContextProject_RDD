12f27fa1d70fe2cb2600a39263ba84549fa65179
mdingjan <M.J.C.Dingjan@student.tudelft.nl>
2016-05-25 22:47:40 +0200

    #67 Added MessageSendDatabaseAdapter tests
    Full coverage for the moment.

---
+++ src/octopeer-github/test/content/DatabaseAdaptable/MessageSendDatabaseAdapterTest.ts
@@ -0,0 +1,22 @@
+/**
+ * Created by Mitchell on 25-5-2016.
+ * Behaviour unit tests for the MessageSendDatabaseAdapter class.
+ */
+
+describe("A MessageSendDatabaseAdapter", function() {
+    let data: IEventObject;
+    let MSDAdapter: DatabaseAdaptable;
+    let spy: jasmine.Spy;
+    let spyObj: jasmine.Spy;
+
+    it("should post data to any database by calling the post method successfully", function () {
+        MSDAdapter = new MessageSendDatabaseAdapter();
+        spy = spyOn(chrome.runtime, "sendMessage");
+        spyObj = jasmine.createSpy("success");
+        data = new EventObject(defaultElementID, defaultEventID, new Date(), 42);
+        MSDAdapter.post(data, spyObj, EMPTY_CALLBACK);
+
+        expect(spy).toHaveBeenCalledWith(JSON.stringify(data));
+        expect(spyObj).toHaveBeenCalled();
+    });
+});
