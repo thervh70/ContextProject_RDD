a313244313bae030088ee9f587f1435130f5ea9b
mdingjan <M.J.C.Dingjan@student.tudelft.nl>
2016-05-25 11:40:26 +0200

    #67 Added tests for new DoNotWatchOptions
    Another PR changed the class via dev, for which I setup some tests. Some of these tests failed and I've added more, so that we reach the full 100% coverage again.

--- src/octopeer-github/test/Options/DoNotWatchOptionsTest.ts
+++ src/octopeer-github/test/Options/DoNotWatchOptionsTest.ts
@@ -5,22 +5,44 @@
 
 describe("The DoNotWatchOptions", function() {
 
-    it("should return elements that should not be watched, yet undefined", function () {
-        expect(DoNotWatchOptions.getElements()).not.toBeDefined();
+    it("should return elements that should not be watched, when comment elements is switched on", function () {
+        spyOn(Options, "getDoNotWatchCommentElements").and.returnValue(true);
+        expect(DoNotWatchOptions.getElements()).toBeDefined();
+        expect(DoNotWatchOptions.getElements()).toEqual([CommentInlineCommentButtonElementSelectionBehaviour,
+            CommentPRButtonElementSelectionBehaviour, EditCommentButtonElementSelectionBehaviour]);
     });
 
-    it("should return events that should not be watched, when the focus is switched off", function () {
-        expect(DoNotWatchOptions.getEvents()).toBeDefined();
-        expect(DoNotWatchOptions.getEvents()).toEqual([]);
+    it("should return elements that should not be watched, when comment elements is switched off", function () {
+        spyOn(Options, "getDoNotWatchCommentElements").and.returnValue(false);
+        expect(DoNotWatchOptions.getElements()).toBeDefined();
+        expect(DoNotWatchOptions.getElements()).toEqual([]);
     });
 
-    it("should return events that should not be watched, when the focus is switched on", function () {
-        spyOn(Options, "getFocus").and.returnValue(true);
+    it("should return events that should not be watched, when on screen events are switched on", function () {
+        spyOn(Options, "getDoNotWatchOnScreenEvents").and.returnValue(true);
         expect(DoNotWatchOptions.getEvents()).toBeDefined();
         expect(DoNotWatchOptions.getEvents()).toEqual([ScrollIntoViewElementEventBinding, ScrollOutOfViewElementEventBinding]);
     });
 
-    it("should return combinations that should not be watched, yet undefined", function () {
-        expect(DoNotWatchOptions.getCombinations()).not.toBeDefined();
+    it("should return events that should not be watched, when the on screen events are switched on", function () {
+        spyOn(Options, "getDoNotWatchHoverEvents").and.returnValue(true);
+        expect(DoNotWatchOptions.getEvents()).toBeDefined();
+        expect(DoNotWatchOptions.getEvents()).toEqual([MouseEnterElementEventBinding, MouseLeaveElementEventBinding]);
+    });
+
+    it("should return events that should not be watched, when the keyboard shortcut events are switched on", function () {
+        spyOn(Options, "getDoNotWatchKeyboardShortcutEvents").and.returnValue(true);
+        expect(DoNotWatchOptions.getEvents()).toBeDefined();
+        expect(DoNotWatchOptions.getEvents()).toEqual([KeystrokeElementEventBinding]);
+    });
+
+    it("should return events that should not be watched, when all events options are switched off", function () {
+        expect(DoNotWatchOptions.getEvents()).toBeDefined();
+        expect(DoNotWatchOptions.getEvents()).toEqual([]);
+    });
+
+    it("should return combinations that should not be watched, yet a placeholder", function () {
+        expect(DoNotWatchOptions.getCombinations()).toBeDefined();
+        expect(DoNotWatchOptions.getCombinations()).toEqual([]);
     });
 });
