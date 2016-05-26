1e3a3a50d09f66c1c1b54ec99190300696aea316
Maarten Sijm <M.P.Sijm@student.tudelft.nl>
2016-05-26 22:05:24 +0200

    #64 Fix copy-pasto in JSDoc

--- src/octopeer-github/main/DatabaseAdaptable/IMouseClickEvent.ts
+++ src/octopeer-github/main/DatabaseAdaptable/IMouseClickEvent.ts
@@ -3,7 +3,7 @@
  */
 
 /**
- * An IKeystrokeEvent contains the data that should be posted to a Database.
+ * An IMouseClickEvent contains the data that should be posted to a Database.
  */
 interface IMouseClickEvent {
     timestamp: UnixTimestamp;
