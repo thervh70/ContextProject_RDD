ccf3bdce1dafebd19629687c945c3ce7dab4bcfb
mdingjan <M.J.C.Dingjan@student.tudelft.nl>
2016-05-26 09:22:04 +0200

    #67 Added an ElementID test
    The ElementID class is now fully covered.

--- src/octopeer-github/test/content/Types/ElementIDTest.ts
+++ src/octopeer-github/test/content/Types/ElementIDTest.ts
@@ -1,13 +1,22 @@
 /**
- * Created by Youri on 08/05/2016.
+ * Created by Youri and Mitchell on 08/05/2016.
+ * Behaviour unit tests for the ElementID class.
  */
 const numericalValueforElementID = 4; // randomly chosen.
 
 describe("An Identifier for an element", function() {
     let elementID: ElementID;
 
-    it("should return the value of instantiation", function () {
+    beforeEach(function() {
         elementID = new ElementID(numericalValueforElementID);
+    });
+
+    it("should return the value of instantiation", function () {
         expect(elementID.getElementID()).toBe(numericalValueforElementID);
+    });
+
+    it("should return a string of the elementID, when toString is called", function () {
+        // the toBe value is a String of the Number 4.
+        expect(elementID.toString()).toBe(numericalValueforElementID.toString());
     });
 });
