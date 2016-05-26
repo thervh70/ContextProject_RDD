e001d07cb035b1a60a11c3921a1dc3e06add6c58
Maarten Sijm <M.P.Sijm@student.tudelft.nl>
2016-05-26 19:26:41 +0200

    Write test for Array.equals

--- src/octopeer-github/main/_ref.ts
+++ src/octopeer-github/main/_ref.ts
@@ -3,16 +3,3 @@
 /// <reference path="../content/Types/EventID.ts"/>
 /// <reference path="../content/ElementSelectionBehaviour/ElementSelectionBehaviour.ts"/>
 /// <reference path="../content/ElementEventBinding/ElementEventBinding.ts"/>
-
-// An interface for defining an equals function for two arrays.
-interface Array<T> {
-    equals(other: Array<T>): boolean;
-}
-
-// Checks whether two arrays are equal.
-Array.prototype.equals = function(array: Array<any>) {
-    return this.length === array.length &&
-        this.every( function(this_i: any, i: any) {
-            return this_i === array[i];
-        });
-};
