80aa343891058f44da34453b02fbd779c6c8e1dc
Mathias Meuleman <m.meuleman@student.tudelft.nl>
2016-05-27 10:25:12 +0200

    #65 Fix MousePositionTracker

--- src/octopeer-github/content/EventTracker/MousePositionTracker.ts
+++ src/octopeer-github/content/EventTracker/MousePositionTracker.ts
@@ -9,21 +9,21 @@ class MousePositionTracker {
      */
     constructor(private db: MousePositionDatabaseAdaptable) {
         const self = this;
-        const $window = $(window);
-        $(document).one("mousemove", function (eventObject: JQueryEventObject) {
-            let mouseX: number;
-            let mouseY: number;
-            let viewportX: number;
-            let viewportY: number;
-            setInterval(function () {
+        let $window: JQuery;
+        let mouseX: number;
+        let mouseY: number;
+        let viewportX: number;
+        let viewportY: number;
+        setInterval(function () {
+            $(document).one("mousemove", function (eventObject: JQueryEventObject) {
+                $window = $(window);
                 mouseX = eventObject.pageX;
                 mouseY = eventObject.pageY;
                 viewportX = $window.scrollLeft();
                 viewportY = $window.scrollTop();
                 self.db.postMousePosition(new MousePositionEvent(viewportX + mouseX, viewportY + mouseY,
                     viewportX, viewportY, new Date().getTime()), EMPTY_CALLBACK, EMPTY_CALLBACK);
-            }, 500);
-        });
+            });
+        }, 500);
     }
 }
-
