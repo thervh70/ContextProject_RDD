4aed6d1d3a6e25e88ac976264dbe2aaecc70e678
thervh70 <r.h.vanheukelum@student.tudelft.nl>
2016-05-30 16:27:50 +0200

    #106 Simple raw data console filter

--- src/octopeer-github/main/Database/ConsoleLogDatabaseAdapter.ts
+++ src/octopeer-github/main/Database/ConsoleLogDatabaseAdapter.ts
@@ -1,10 +1,29 @@
 /**
  * Created by Youri on 04/05/2016.
- */
-/**
  * Semi-mock class for being able to check implementation completeness.
  */
 class ConsoleLogDatabaseAdapter implements DatabaseAdaptable {
+
+    /**
+     * Simple boolean to keep track of the raw data output.
+     */
+    private static outputRawData = true;
+
+    /**
+     * Set the raw data switch for the console.
+     * @bool the new value.
+     */
+    public static setRawData(bool: boolean) {
+        ConsoleLogDatabaseAdapter.outputRawData = bool;
+    }
+
+    /**
+     * Get raw data value.
+     * @returns raw data value.
+     */
+    public static getRawData() {
+        return ConsoleLogDatabaseAdapter.outputRawData;
+    }
 
     /**
      * @param eventData The data to log to the console.
@@ -17,35 +36,44 @@ class ConsoleLogDatabaseAdapter implements DatabaseAdaptable {
      * @param eventData The data to log to the console.
      */
     public postKeystroke(eventData: IKeystrokeEvent) {
-        Logger.database(eventData);
+        this.filterRawData(eventData);
     }
 
     /**
      * @param eventData The data to log to the console.
      */
     public postMousePosition(eventData: IMousePositionEvent) {
-        Logger.database(eventData);
+        this.filterRawData(eventData);
     }
 
     /**
      * @param eventData The data to log to the console.
      */
     public postMouseClick(eventData: IMouseClickEvent) {
-        Logger.database(eventData);
+        this.filterRawData(eventData);
     }
 
     /**
      * @param eventData The data to log to the console.
      */
     public postMouseScroll(eventData: IMouseScrollEvent) {
-        Logger.database(eventData);
+        this.filterRawData(eventData);
     }
 
     /**
      * @param eventData The data to log to the console.
      */
     public postWindowResolution(eventData: IWindowResolutionEvent) {
-        Logger.database(eventData);
+        this.filterRawData(eventData);
+    }
+
+    /**
+     * Raw data filter method.
+     */
+    public filterRawData(eventData: any) {
+        if (ConsoleLogDatabaseAdapter.outputRawData) {
+            Logger.database(eventData);
+        }
     }
 
 }
