850fb2585b9d3286f92ef6d11bd1a6a14ba3c243
mdingjan <M.J.C.Dingjan@student.tudelft.nl>
2016-05-30 12:06:07 +0200

    Fixed some typos in the documentation.
    In Options.

--- src/octopeer-github/main/Options/Options.ts
+++ src/octopeer-github/main/Options/Options.ts
@@ -184,7 +184,7 @@ const Options = new (class Options {
 
     /**
      * Gets doNotWatch preference about onscreen elements, from the chrome storage.
-     * User persepective: Do not watch what elements are on my screen.
+     * User perspective: Do not watch what elements are on my screen.
      * @returns {Boolean}
      */
     public getDoNotWatchOnScreenEvents() {
@@ -193,7 +193,7 @@ const Options = new (class Options {
 
     /**
      * Gets doNotWatch preference about hovering above elements, from the chrome storage.
-     * User persepective: Do not watch what elements I hover over.
+     * User perspective: Do not watch what elements I hover over.
      * @returns {Boolean}
      */
     public getDoNotWatchHoverEvents() {
@@ -202,7 +202,7 @@ const Options = new (class Options {
 
     /**
      * Gets doNotWatch preference about comments, from the chrome storage.
-     * User persepective: Do not watch the comments of my pull request.
+     * User perspective: Do not watch the comments of my pull request.
      * @returns {Boolean}
      */
     public getDoNotWatchCommentElements() {
@@ -211,7 +211,7 @@ const Options = new (class Options {
 
     /**
      * Gets doNotWatch preference about keyboard shortcuts, from the chrome storage.
-     * User persepective: Do not watch my keyboard shortcuts.
+     * User perspective: Do not watch my keyboard shortcuts.
      * @returns {Boolean}
      */
     public getDoNotWatchKeyboardShortcutEvents() {
