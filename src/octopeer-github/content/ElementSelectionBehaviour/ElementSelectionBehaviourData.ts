f1c1bf4ec3873d97ef22d415461bd07bc59b72d6
Mathias Meuleman <m.meuleman@student.tudelft.nl>
2016-05-30 12:46:42 +0200

    #95 Add ESBData interface and PageMask template

---
+++ src/octopeer-github/content/ElementSelectionBehaviour/ElementSelectionBehaviourData.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by Mathias on 30-05-2016.
+ * This interface enforces the
+ */
+interface ElementSelectionBehaviourData {
+    elementID: ElementID;
+    name: string;
+    selector: string;
+    composedSelector: () => JQuery;
+    foundOnPages: PageMask;
+    callback: (event: JQueryEventObject) => void;
+}
