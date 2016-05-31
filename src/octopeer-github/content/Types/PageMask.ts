1ee56760c5acd40d94e872c78e9e1095a78ad65c
Maarten Sijm <M.P.Sijm@student.tudelft.nl>
2016-05-31 09:26:48 +0200

    #95 Add first implementation of PageMask

--- src/octopeer-github/content/Types/PageMask.ts
+++ src/octopeer-github/content/Types/PageMask.ts
@@ -1,3 +1,21 @@
+/**
+ * Created by Maarten on 30-05-2016
+ *
+ * Class for handling PageMasks.
+ * PageMasks indicate which elements are on which pages.
+ */
+// tslint:disable:no-bitwise
 class PageMask {
 
+    public static get CONVERSATION()    { return new PageMask(0b001); };
+    public static get COMMITS()         { return new PageMask(0b010); };
+    public static get FILES_CHANGED()   { return new PageMask(0b100); };
+
+    constructor(private _mask: number) { }
+
+    public isOnPage(pageMask: PageMask): boolean {
+        return this._mask ^ pageMask._mask > 0;
+    }
+
 }
+// tslint:enable:no-bitwise
