535c6801536464f33b5808ce5655954c90e713cd
Maarten Sijm <M.P.Sijm@student.tudelft.nl>
2016-05-31 22:46:15 +0200

    #64 Remove TODO

--- src/octopeer-github/main/MainController.ts
+++ src/octopeer-github/main/MainController.ts
@@ -90,8 +90,8 @@ class MainController implements OptionsObserver {
                 return; // Only continue if message is sent from a content script
             }
             const dataMessage = <DataMessage>JSON.parse(message);
+            // TODO: get name from context
             const database: DatabaseAdaptable = new RESTApiDatabaseAdapter("http://10.0.22.6", sender.tab.url, "Travis");
-            // TODO: change to RESTApiDatabaseAdapter("http://10.0.22.6", sender.tab.url, "Travis"); // TODO: get name from context
             const success = function() {
                 Logger.debug(`Successfully logged to database: ${message}`);
             };
