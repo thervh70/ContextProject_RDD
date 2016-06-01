e748da7d372cdd8c763c9bc6652c1631e69efc73
mdingjan <M.J.C.Dingjan@student.tudelft.nl>
2016-06-01 22:01:52 +0200

    #94 Adapted DoNotWatchOptionsTest
    Because of the refactoring, some changes had to be introduced in the tests.
    A callFake is applied to the methods that are dependent of getOption, as all Option getters are reduced to one.

--- src/octopeer-github/test/main/Options/DoNotWatchOptionsTest.ts
+++ src/octopeer-github/test/main/Options/DoNotWatchOptionsTest.ts
@@ -6,32 +6,59 @@
 describe("The DoNotWatchOptions", function() {
 
     it("should return elements that should not be watched, when comment elements is switched on", function () {
-        spyOn(Options, "getDoNotWatchCommentElements").and.returnValue(true);
+        spyOn(Options, "getOption").and.returnValue(true);
         expect(DoNotWatchOptions.getElements()).toBeDefined();
         expect(DoNotWatchOptions.getElements()).toEqual([CommentInlineCommentButtonElementSelectionBehaviour,
             CommentPRButtonElementSelectionBehaviour, EditCommentButtonElementSelectionBehaviour]);
     });
 
     it("should return elements that should not be watched, when comment elements is switched off", function () {
-        spyOn(Options, "getDoNotWatchCommentElements").and.returnValue(false);
+        spyOn(Options, "getOption").and.returnValue(false);
         expect(DoNotWatchOptions.getElements()).toBeDefined();
         expect(DoNotWatchOptions.getElements()).toEqual([]);
     });
 
     it("should return events that should not be watched, when on screen events are switched on", function () {
-        spyOn(Options, "getDoNotWatchOnScreenEvents").and.returnValue(true);
+        spyOn(Options, "getOption").and.callFake(function(optionName: string) {
+            if (optionName === "doNotWatchOnScreenEvents") {
+                return true;
+            } else if (optionName === "doNotWatchHoverEvents" || optionName === "doNotWatchKeyboardShortcutEvents") {
+                return false;
+            } else {
+                // else a non-existing string is used, so throw an error.
+                throw EvalError;
+            }
+        });
         expect(DoNotWatchOptions.getEvents()).toBeDefined();
         expect(DoNotWatchOptions.getEvents()).toEqual([ScrollIntoViewElementEventBinding, ScrollOutOfViewElementEventBinding]);
     });
 
     it("should return events that should not be watched, when the on screen events are switched on", function () {
-        spyOn(Options, "getDoNotWatchHoverEvents").and.returnValue(true);
+        spyOn(Options, "getOption").and.callFake(function(optionName: string) {
+            if (optionName === "doNotWatchHoverEvents") {
+                return true;
+            } else if (optionName === "doNotWatchOnScreenEvents" || optionName === "doNotWatchKeyboardShortcutEvents") {
+                return false;
+            } else {
+                // else a non-existing string is used, so throw an error.
+                throw EvalError;
+            }
+        });
         expect(DoNotWatchOptions.getEvents()).toBeDefined();
         expect(DoNotWatchOptions.getEvents()).toEqual([MouseEnterElementEventBinding, MouseLeaveElementEventBinding]);
     });
 
     it("should return events that should not be watched, when the keyboard shortcut events are switched on", function () {
-        spyOn(Options, "getDoNotWatchKeyboardShortcutEvents").and.returnValue(true);
+        spyOn(Options, "getOption").and.callFake(function(optionName: string) {
+            if (optionName === "doNotWatchKeyboardShortcutEvents") {
+                return true;
+            } else if (optionName === "doNotWatchOnScreenEvents" || optionName === "doNotWatchHoverEvents") {
+                return false;
+            } else {
+                // else a non-existing string is used, so throw an error.
+                throw EvalError;
+            }
+        });
         expect(DoNotWatchOptions.getEvents()).toBeDefined();
         expect(DoNotWatchOptions.getEvents()).toEqual([KeystrokeElementEventBinding]);
     });
