1c6cf93ad2db7a25511c8bd091c30d2c995f9350
Mathias Meuleman <m.meuleman@student.tudelft.nl>
2016-06-02 16:33:56 +0200

    #95 Add missing TSDoc to ESBFactory

--- src/octopeer-github/content/ElementSelectionBehaviour/ElementSelectionBehaviourFactory.ts
+++ src/octopeer-github/content/ElementSelectionBehaviour/ElementSelectionBehaviourFactory.ts
@@ -1,6 +1,7 @@
 /// <reference path="GenericElementSelectionBehaviour.ts"/>
 /**
  * Created by Mathias on 2016-05-31.
+ * Used to create GenericElementSelectionBehaviours based on their ElementID.
  */
 class ElementSelectionBehaviourFactory {
 
