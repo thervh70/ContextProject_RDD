3b53cd8cb22bd47435f2f2e3e43cfa38926be6a8
Maarten Sijm <M.P.Sijm@student.tudelft.nl>
2016-06-02 15:08:02 +0200

    Add ElementID.equals

--- src/octopeer-github/test/content/Types/ElementIDTest.ts
+++ src/octopeer-github/test/content/Types/ElementIDTest.ts
@@ -11,12 +11,20 @@ describe("An Identifier for an element", function() {
         elementID = new ElementID(numericalValueforElementID);
     });
 
-    it("should return the value of instantiation", function () {
+    it("should return the value of instantiation", function() {
         expect(elementID.getElementID()).toBe(numericalValueforElementID);
     });
 
-    it("should return a string of the elementID, when toString is called", function () {
+    it("should return a string of the elementID, when toString is called", function() {
         // the toBe value is a String of the Number 4.
         expect(elementID.toString()).toBe(numericalValueforElementID.toString());
+    });
+
+    it("should say that two equivalent elementIDs are equal", function() {
+        expect(elementID.equals(new ElementID(numericalValueforElementID))).toBe(true);
+    });
+
+    it("should say that two different elementIDs are not equal", function() {
+        expect(elementID.equals(new ElementID(numericalValueforElementID + 1))).toBe(false);
     });
 });
