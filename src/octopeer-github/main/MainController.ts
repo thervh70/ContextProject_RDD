46c217cb3684181d75a5f0ceee47c29d84c7fca5
mdingjan <M.J.C.Dingjan@student.tudelft.nl>
2016-06-02 09:49:14 +0200

    #94 Removed the update/init TODO
    This change has been applied, so the TODO has been removed.

--- src/octopeer-github/main/MainController.ts
+++ src/octopeer-github/main/MainController.ts
@@ -15,7 +15,6 @@ class MainController implements OptionsObserver {
         Logger.setDebug(); // TODO remove this on release
         this.connectToContentScript();
         Status.standby();
-        // TODO: Options.update actually means "add a listener to the storage" so can be merged into init
         Options.init();
         Options.addObserver(this);
         return this;
