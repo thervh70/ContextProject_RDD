4a4385e010d22d5ec6e48cb88fb51783c67da1dd
mdingjan <M.J.C.Dingjan@student.tudelft.nl>
2016-06-08 23:01:02 +0200

    #117 Adapted naming in popup

--- resources/popup.js
+++ resources/popup.js
@@ -1,27 +1,27 @@
+// Use the toggle button on the popup to switch the loggingEnabled option.
+function toggle() {
+    chrome.storage.sync.get("loggingEnabled", function (res) {
+        chrome.storage.sync.set({
+            loggingEnabled: !(res.loggingEnabled)
+        })
+    });
+}
+
 $(document).ready(function () {
-    //Get from storage on start.
+    // Get from storage on start.
     chrome.storage.local.get(["line", "path"], function(items) {
-        $("#status_text").html(items.line);
-        $("#status_image").attr("src", items.path);
+        $("#statusText").html(items.line);
+        $("#statusImage").attr("src", items.path);
     });
 
-    //Reset status on change.
+    // Reset status on change.
     chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
         if (message.line !== undefined) {
-            $("#status_text").html(message.line);
-            $("#status_image").attr("src", message.path);
+            $("#statusText").html(message.line);
+            $("#statusImage").attr("src", message.path);
         }
         sendResponse({});
     });
 
-    //Use the toggle button on the popup to switch the loggingEnabled option.
-    function toggle() {
-        chrome.storage.sync.get("loggingEnabled", function (res) {
-            chrome.storage.sync.set({
-                loggingEnabled: !(res.loggingEnabled)
-            })
-        });
-    }
-
-    $('#status_toggle').click(toggle);
+    $('#statusToggle').click(toggle);
 });
