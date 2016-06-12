5b3621263467883e34fcc8b0d58b66b1a6afdc8e
Maarten Sijm <M.P.Sijm@student.tudelft.nl>
2016-06-12 17:26:55 +0200

    #134 Change DOMRewriter from static class to normal class

--- src/octopeer-github/content/Database/DOMRewriter.ts
+++ src/octopeer-github/content/Database/DOMRewriter.ts
@@ -2,9 +2,8 @@
  * Created by Maarten on 12-06-2016.
  *
  * The DOMRewriter can be used to put octopeer tags everywhere.
- * It should be final, but TypeScript does not support this yet.
  */
-abstract class DOMRewriter {
+class DOMRewriter {
 
     /** The prefix that is used for all octopeer tags. */
     public static get PREFIX() { return "data-octopeer-"; }
@@ -13,7 +12,7 @@ abstract class DOMRewriter {
      * Rewrites the node given as parameter and all of its children in the subtree by adding tags.
      * @param node The node to rewrite
      */
-    public static rewrite(node: JQuery) {
+    public rewrite(node: JQuery) {
         const nodePosition = node.position();
         node.attr(`${DOMRewriter.PREFIX}x`, nodePosition.left);
         node.attr(`${DOMRewriter.PREFIX}y`, nodePosition.top);
@@ -21,7 +20,7 @@ abstract class DOMRewriter {
         node.attr(`${DOMRewriter.PREFIX}height`, node.height());
         // TODO z-index
         node.children().each(function() {
-            DOMRewriter.rewrite($(this));
+            this.rewrite($(this));
         });
     }
 }
--- src/octopeer-github/test/main/Database/DOMRewriterTest.ts
+++ src/octopeer-github/test/main/Database/DOMRewriterTest.ts
@@ -7,7 +7,7 @@ describe("The DOMRewriter", function() {
     it("should rewrite all tags to contain data-octopeer-* tags", function() {
         setFixtures("<div id='root'><div id='topdiv'><p id='par'>Random text</p><button id='but'>Random button</button></div></div>");
         const topDiv = $("#topdiv");
-        DOMRewriter.rewrite(topDiv);
+        new DOMRewriter().rewrite(topDiv);
 
         for (let suffix of ["x", "y", "width", "height"]) {
             const attrname = `data-octopeer-${suffix}`;
