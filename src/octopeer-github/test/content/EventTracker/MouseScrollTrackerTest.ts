89aa532d01aaeecb9a5eb4cb43f4876916af4771
Mathias Meuleman <m.meuleman@student.tudelft.nl>
2016-06-15 14:27:02 +0200

    #135 Remove old test

--- src/octopeer-github/test/content/EventTracker/MouseScrollTrackerTest.ts
+++ src/octopeer-github/test/content/EventTracker/MouseScrollTrackerTest.ts
@@ -17,7 +17,7 @@ describe("A MouseScrollTracker", function() {
         jasmine.clock().uninstall();
     });
 
-    it("should track mouse scrolls every second, unless the position didn't change", function() {
+    it("should track mouse scrolls every second when the scroll:finish event fires, unless the position didn't change", function() {
         const dbSpy = spyOn(db, "post");
         tracker.addDOMEvent();
 
@@ -28,19 +28,6 @@ describe("A MouseScrollTracker", function() {
 
         jasmine.clock().tick(MouseScrollTracker.TIMEOUT + 1);
         expect(dbSpy).toHaveBeenCalledTimes(1); // this is the total amount of calls, meaning no new call has been made
-    });
-
-    xit("should track only one mouse scroll event, when the times between them are less than a second", function() {
-        const dbSpy = spyOn(db, "post");
-        tracker.addDOMEvent();
-
-        $(window).trigger($.Event("scroll:finish", {pageX: 80, pageY: 80}));
-        jasmine.clock().tick(MouseScrollTracker.TIMEOUT / 2);
-
-        $(window).trigger($.Event("scroll:finish", {pageX: 80, pageY: 80}));
-        jasmine.clock().tick(MouseScrollTracker.TIMEOUT + 1);
-
-        expect(dbSpy).toHaveBeenCalledTimes(1);
     });
 
     it("should no longer track mouse positions when removed from DOM", function() {
