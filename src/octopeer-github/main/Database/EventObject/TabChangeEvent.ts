f3efa538e331ae0de21c3165bc1d3e370b998356
Mathias Meuleman <m.meuleman@student.tudelft.nl>
2016-06-15 14:56:17 +0200

    #140 Add TabChangeEvent interface

---
+++ src/octopeer-github/main/Database/EventObject/TabChangeEvent.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by Mathias on 2016-06-15.
+ */
+
+/**
+ * A TabChangeEvent contains the tab change data that shoud be posted to the database.
+ * @param timestamp     when the event was created.
+ */
+interface TabChangeEvent {
+    created_at: UnixTimestamp;
+}
