8b2730870d88e3bf76ecb0ed8c0de7cd2556b127
Mathias Meuleman <m.meuleman@student.tudelft.nl>
2016-06-15 20:03:47 +0200

    #138 Refactor setPopupTarget in front-end

--- resources/main.js
+++ resources/main.js
@@ -5,10 +5,14 @@ $(document).ready(function () {
         closeOnClick: true
     });
 
+    setPopupTarget();
+});
+
+function setPopupTarget() {
     $('#analytics-link').on('click', function() {
         var target = $('head meta[name=analytics-browser-action]').attr('content');
         chrome.storage.local.get('user', function(result) {
             window.open('../analytics/index.html?platform=GitHub&userName=' + result.user, target);
         })
     })
-});
+}
