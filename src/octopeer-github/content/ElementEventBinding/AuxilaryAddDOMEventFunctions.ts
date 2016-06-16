b60485e8eeab2281fd2183a1b9bb1b4488605eae
Mathias Meuleman <m.meuleman@student.tudelft.nl>
2016-06-16 15:03:45 +0200

    #135 Fix TSDoc

--- src/octopeer-github/content/ElementEventBinding/AuxilaryAddDOMEventFunctions.ts
+++ src/octopeer-github/content/ElementEventBinding/AuxilaryAddDOMEventFunctions.ts
@@ -11,7 +11,7 @@ class AuxiliaryAddDOMEventFunctions {
      * @param width the width of the scope
      * @param height the height of the scope
      * @param rectangle the rectangle to be checked
-     * @returns {boolean}
+     * @returns {boolean} true when both in horizontal and vertical scope, else false
      */
     public static isInScope (width: number, height: number, rectangle: ClientRect) {
         return this.isInHorizontalScope(width, rectangle) && this.isInVerticalScope(height, rectangle);
@@ -19,10 +19,10 @@ class AuxiliaryAddDOMEventFunctions {
 
     /**
      * Determine whether a given number is in between two other numbers (same number is also inBetween)
-     * @param leftBound the leftbound
-     * @param rightBound the rightbound
+     * @param leftBound the left bound
+     * @param rightBound the right bound
      * @param value the number to be checked
-     * @returns {boolean}
+     * @returns {boolean} true if given value in between (or same as) given bounds, else false
      */
     private static isBetween (leftBound: number, rightBound: number, value: number) {
         return leftBound <= value && rightBound >= value;
@@ -32,7 +32,7 @@ class AuxiliaryAddDOMEventFunctions {
      * Check whether a rectangle is between two values, horizontally.
      * @param width the width between the two values
      * @param rect the rectangle to be checked
-     * @returns {boolean}
+     * @returns {boolean} true when in horizontal scope, else false
      */
     private static isInHorizontalScope (width: number, rect: ClientRect) {
         return this.isBetween(0, width, rect.left) || this.isBetween(0, width, rect.right);
@@ -42,7 +42,7 @@ class AuxiliaryAddDOMEventFunctions {
      * Check whether a rectangle is between two values, vertically.
      * @param height the height between the two values
      * @param rect the rectangle to be checked
-     * @returns {boolean}
+     * @returns {boolean} true when in vertical scope, else false
      */
     private static isInVerticalScope (height: number, rect: ClientRect) {
         return this.isBetween(0, height, rect.top) || this.isBetween(0, height, rect.bottom);
