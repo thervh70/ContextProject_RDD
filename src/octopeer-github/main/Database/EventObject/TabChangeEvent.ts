f0a82a98c666f54801ec49476aaa054ea7b19a08
Mathias Meuleman <m.meuleman@student.tudelft.nl>
2016-06-16 22:59:22 +0200

    #140 Fix some TSDoc

--- src/octopeer-github/main/Database/EventObject/TabChangeEvent.ts
+++ src/octopeer-github/main/Database/EventObject/TabChangeEvent.ts
@@ -1,8 +1,6 @@
 /**
  * Created by Mathias on 2016-06-15.
- */
-
-/**
+ * 
  * A TabChangeEvent contains the tab change data that shoud be posted to the database.
  * @param timestamp     when the event was created.
  */
