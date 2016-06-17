59aa318ab3c51b2f7386159e381a1a89b05bb915
mpsijm <M.P.Sijm@student.tudelft.nl>
2016-06-17 16:27:35 +0200

    #159 Move commit hash in test to const

--- src/octopeer-github/test/content/ElementSelectionBehaviour/DiffElementSelectionBehaviourFunctionsTest.ts
+++ src/octopeer-github/test/content/ElementSelectionBehaviour/DiffElementSelectionBehaviourFunctionsTest.ts
@@ -12,9 +12,10 @@ describe("The DiffElementSelectionBehaviourFunctions", function() {
     });
 
     it("should return the right commit id", function() {
+        const commitHashFromTestHTML = "39efa2e95a60e240861dc85028d90f93aef7eabc";
         loadFixtures("files-changed-tab.html");
         expect(DiffElementSelectionBehaviourFunctions.getCommitHashFromDiffLine($(".blob-num-addition")))
-            .toEqual("39efa2e95a60e240861dc85028d90f93aef7eabc");
+            .toEqual(commitHashFromTestHTML);
     });
 
 });
